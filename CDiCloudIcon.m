6a598f0ddb15e6c03ae8c62858ee408ba104900a
Rob Stearn <robstearn@me.com>
2012-07-18 17:23:48 +0100

    Edited to inset edges based on line thickness
    Now the cloud always draws completely inside the bounding box based on
    a dynamic assessment of the line thickness.

--- CDiCloudIcon.m
+++ CDiCloudIcon.m
@@ -1,13 +1,11 @@
 //
 //  CDiCloudIcon.m
 //
-//  Created on 17/07/2012.
+//  Created by Robert Stearn on 17/07/2012.
 //  Copyright (c) 2012 Cocoadelica. All rights reserved.
 //
 
 #import "CDiCloudIcon.h"
-#define GMX CGRectGetMaxX(self.bounds)
-#define GMY CGRectGetMaxY(self.bounds)
 
 @implementation CDiCloudIcon
 
@@ -28,14 +26,21 @@
 	CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 1.0);
 	
     UIBezierPath *cloudPath = [UIBezierPath bezierPath];
-	[cloudPath addArcWithCenter:CGPointMake(GMX * 0.166666667, GMY * 0.7222) radius:GMX * 0.166666667 startAngle:1.574 endAngle:4.721 clockwise:YES];
-	[cloudPath addArcWithCenter:CGPointMake(GMX * 0.2708, GMY * 0.4444) radius:GMX * 0.1 startAngle:3.147 endAngle:5.245 clockwise:YES];
-	[cloudPath addArcWithCenter:CGPointMake(GMX * 0.5667, GMY * 0.44445) radius:GMY * 0.44445 startAngle:3.672 endAngle:0.0001 clockwise:YES];
-	[cloudPath addArcWithCenter:CGPointMake(GMX * 0.833333333, GMY * 0.7222) radius:GMX * 0.166666667 startAngle:4.721 endAngle:1.574 clockwise:YES];
-	[cloudPath setLineWidth:0.5];
+	[cloudPath setLineWidth:5];
+	
+	//inset the drawing rect by the line width * 0.5. Oh yeah.
+	CGRect clipRect = CGRectMake(cloudPath.lineWidth, cloudPath.lineWidth, rect.size.width - cloudPath.lineWidth , rect.size.height - cloudPath.lineWidth);
+	CGFloat maxX = clipRect.size.width;
+	CGFloat maxY = clipRect.size.height;
+	CGFloat offset = cloudPath.lineWidth * 0.5;
+	[cloudPath addArcWithCenter:CGPointMake(maxX * 0.166666667 + offset, maxY * 0.7222 + offset) radius:maxX * 0.166666667 startAngle:1.574 endAngle:4.721 clockwise:YES];
+	[cloudPath addArcWithCenter:CGPointMake(maxX * 0.2708 + offset, maxY * 0.4444 + offset) radius:maxX * 0.1 startAngle:3.3 endAngle:5.245 clockwise:YES];
+	[cloudPath addArcWithCenter:CGPointMake(maxX * 0.5667 + offset, maxY * 0.44445 + offset) radius:maxY * 0.44445 startAngle:3.672 endAngle:0.0001 clockwise:YES];
+	[cloudPath addArcWithCenter:CGPointMake(maxX * 0.833333333 + offset, maxY * 0.7222 + offset) radius:maxX * 0.166666667 startAngle:4.721 endAngle:1.574 clockwise:YES];
+	
 	[cloudPath closePath];
 	[cloudPath stroke];
-	[cloudPath fill];
+	//[cloudPath fill]; optional fill
 }
 
 
